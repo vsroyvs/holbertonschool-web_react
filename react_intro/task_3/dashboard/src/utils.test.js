4c74c1bf45c85c925f523607a583b4412f75e469
Roy <vargas.sap321@gmail.com>
2023-11-23 00:17:56 -0500

    Create basic tests with four tests

---
+++ react_intro/task_3/dashboard/src/utils.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { getFullYear, getFooterCopy, getLatestNotification } from  './utils';
+
+it('getFullYear should return the current year', () => {
+    const currentYear = new Date().getFullYear();
+    expect(getFullYear()).toEqual(currentYear);
+});
+
+test("getFooterCopy returns the correct string when the argument is true", () => {
+    expect(getFooterCopy(true)).toEqual("Holberton School");
+});
+
+test("getFooterCopy returns the correct string when the argument is false", () => {
+    expect(getFooterCopy(false)).toEqual("Holberton School main dashboard");
+});
+
+test("getLatestNotification returns the expected string", () => {
+    expect(getLatestNotification()).toEqual("<strong>Urgent requirement</strong> - complete by EOD");
+});
+
