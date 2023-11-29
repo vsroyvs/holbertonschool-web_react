62115defccdaa65dca171fb7e7698d2a919fc21a
Roy <vargas.sap321@gmail.com>
2023-11-29 16:17:47 -0500

    Create React tests

---
+++ react_intro/task_3/dashboard/src/App.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe("App Component", () =>{
+    it('renders app without crash',() => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    //'App-header' existe en App.
+    it('is App-header', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('.App-header').exists()).toBe(true);
+    });
+
+    //'App-body' existe en App.
+    it('is App-body', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('.App-body').exists()).toBe(true);
+    });
+
+    //App-footer' existe en App.
+    it('is App-footer', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('.App-footer').exists()).toBe(true);
+    });
+});
